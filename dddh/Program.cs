7766a761409395901418f4218645e6ad8882a014
Deirror <alexmailbg@gmail.com>
2023-06-14 16:12:53 +0300

    commit

--- dddh/Program.cs
+++ dddh/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Dynamic;
+
+namespace dddh // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the parameters of the two sides!");
+            decimal a = decimal.Parse(Console.ReadLine());
+            decimal b = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(GetRectangleArea(a,b)); 
+
+        }
+
+        public static decimal GetRectangleArea(decimal a, decimal b)
+        {
+            return a*b;
+        }
+    }
+}
