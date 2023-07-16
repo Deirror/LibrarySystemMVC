ae76c8eeb81a46c018a442e53ba268b221757199
Deirror <136594831+Deirror@users.noreply.github.com>
2023-07-16 13:32:10 +0300

    Update AccessDeniedController.cs

--- LibrarysSystem/Controllers/AccessDeniedController.cs
+++ LibrarysSystem/Controllers/AccessDeniedController.cs
@@ -1,13 +1,1 @@
-﻿using Microsoft.AspNetCore.Mvc;
 
-namespace LibrarysSystem.Controllers
-{
-    public class AccessDeniedController : Controller
-    {
-        [HttpGet]
-        public IActionResult AccessDeniedPage()
-        {
-            return View();
-        }
-    }
-}
